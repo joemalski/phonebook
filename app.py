0d4399f04b5c5219d19e3340444e8ea93df31023
Joel Malinao <joe.malski@gmail.com>
2019-06-05 11:30:51 +0800

    add dummy list on main page

--- app.py
+++ app.py
@@ -12,6 +12,20 @@ def main(stdscr):
     curses.curs_set(False)
 
     skins.main(stdscr)
+
+    records = Utility.get_records(10)
+    stdscr.addstr(3, 25,  "ID  Name                    Phone")
+    stdscr.addstr(4, 25,  "{}   {}                {}".format(records[0]['id'], records[0]['name'], records[0]['phone']))
+    stdscr.addstr(5, 25,  "{}   {}                {}".format(records[1]['id'], records[1]['name'], records[1]['phone']))
+    stdscr.addstr(6, 25,  "{}   {}                {}".format(records[2]['id'], records[2]['name'], records[2]['phone']))
+    stdscr.addstr(7, 25,  "{}   {}                {}".format(records[3]['id'], records[3]['name'], records[3]['phone']))
+    stdscr.addstr(8, 25,  "{}   {}                {}".format(records[4]['id'], records[4]['name'], records[4]['phone']))
+    stdscr.addstr(9, 25,  "{}   {}                {}".format(records[5]['id'], records[5]['name'], records[5]['phone']))
+    stdscr.addstr(10, 25, "{}   {}                {}".format(records[6]['id'], records[6]['name'], records[6]['phone']))
+    stdscr.addstr(11, 25, "{}   {}                {}".format(records[7]['id'], records[7]['name'], records[7]['phone']))
+    stdscr.addstr(12, 25, "{}   {}                {}".format(records[8]['id'], records[8]['name'], records[8]['phone']))
+    stdscr.addstr(13, 25, "{}   {}                {}".format(records[9]['id'], records[9]['name'], records[9]['phone']))
+
     key = stdscr.getch()
 
     # main event loop
@@ -42,16 +56,11 @@ def main(stdscr):
 
         key = stdscr.getch()
 
-records = Utility.get_records(10)
 
-for record in records:
-    print(record)
-
-'''
 msg = Utility.check_files_exists()
 if msg == True:
     curses.wrapper(main)
 else:
     print(msg)
-'''
+
 print('Exited Phonebook. Bye!')
